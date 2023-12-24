5c92e04e5539998b475e148959f463aab429f4d4
DonnyWhoLovedBowling <p.c.van.der.deijl@gmail.com>
2023-12-24 22:34:07 +0100

    tm ex24

--- src/ex24.py
+++ src/ex24.py
@@ -1,43 +1,16 @@
 import math
-import sympy
+from scipy.optimize import root
 import logging
 from logging import info, debug
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.DEBUG,
+    level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
 in_file = open("../data/ex24.txt")
 lines = [l.replace('\n', '') for l in in_file.readlines()]
 hail_stones = list()
-
-
-def loss_function(h1, h2):
-    # px1 + vx1*t = px2 + vx2*t
-    # t = (px1-px2)/(vx2-vx1)
-
-    dvx = (h2['vx'] - h1['vx'])
-    if dvx != 0:
-        tc = (h1['px'] - h2['px']) / dvx
-        h1_new = calc_collision_point(h1, tc)
-        h2_new = calc_collision_point(h2, tc)
-        return calc_distance(h1_new, h2_new)
-    else:
-        return 1e17
-
-
-def calc_distance(h1: dict, h2: dict):
-    return math.sqrt(pow(h1['px']-h2['px'], 2)+pow(h1['py']-h2['py'], 2)+pow(h1['pz']-h2['pz'], 2))
-    # return h1['px']-h2['px'] + h1['py']-h2['py'] + h1['pz']-h2['pz']
-
-
-def calc_collision_point(h, t):
-    h_new = dict()
-    h_new['px'] = h['px']+h['vx']*t
-    h_new['py'] = h['py']+h['vy']*t
-    h_new['pz'] = h['pz']+h['vz']*t
-    return h_new
 
 
 def calc_intersection_point(h1, h2):
@@ -59,7 +32,7 @@ def calc_intersection_point(h1, h2):
 
 
 def pt1():
-    global hail_stoned
+    global hail_stones
     for i, line in enumerate(lines):
         hail = dict()
         hail['px'], hail['py'], hail['pz'] = tuple(map(int, line.split("@")[0].split(',')))
@@ -88,39 +61,11 @@ def pt1():
 
 
 def optimize_wrapper(args):
-    # start = datetime.now()
-    x, y, z, vx, vy, vz = args
     global hail_stones
-    h_sol = dict()
-    h_sol['px'] = math.floor(x)
-    h_sol['py'] = math.floor(y)
-    h_sol['pz'] = math.floor(z)
-    h_sol['vx'] = math.floor(vx)
-    h_sol['vy'] = math.floor(vy)
-    h_sol['vz'] = math.floor(vz)
-    total_distance = 0
-    collided = 0
-    for h_test in hail_stones:
-        l = loss_function(h_test, h_sol)
-        # debug(l)
-        total_distance += l
-        if total_distance == 0:
-            collided += 1
-    debug(collided)
-    # print(h_sol)
-    # print(total_distance)
-    # end = datetime.now()
-    # elapsed = difference = end - start
-    # print(f"elapsed: {elapsed.microseconds/1000.}" )
-    debug(total_distance)
-    return total_distance
-
-
-def pt2():
-    x, y, z, vx, vy, vz = sympy.symbols("x, y, z, vx, vy, vz")
+    x, y, z, vx, vy, vz = args
     equations = []
 
-    for h in hail_stones:
+    for i, h in enumerate(hail_stones):
         vx1 = h['vx']
         vy1 = h['vy']
         vz1 = h['vz']
@@ -131,9 +76,12 @@ def pt2():
         equations.append((x - x1) * (vy1 - vy) - (y - y1) * (vx1 - vx))
         equations.append((x - x1) * (vz1 - vz) - (z - z1) * (vx1 - vx))
 
-    ans = sympy.solve(equations)
-    debug(ans)
-    info(f"ans pt2 = {ans[0][x]+ans[0][y]+ans[0][z]}")
+    return equations
+
+
+def pt2():
+    result = root(optimize_wrapper, [1e14, 1e14, 1e14, 100, 100, 100], method='lm')
+    info(f"ans pt2 = {int(sum(result['x'][0:3]))}")
 
 
 if __name__ == '__main__':
